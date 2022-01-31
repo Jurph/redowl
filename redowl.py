f7e0ef12d97370aaf03523805b569cbfc7796417
Jurph <j.r.parsons@gmail.com>
2022-01-31 18:54:34 -0500

    Sketched in some core functionality

--- redowl.py
+++ redowl.py
@@ -15,29 +15,46 @@ class Wordle(object):
             self.wordbank = fh.readlines()
         return
 
-    def eval(self, word):
-        # set self.score based on positional letters
-        # 0 for a letter which does not appear
-        # 1 for a letter which appears in a different position
-        # 2 for a letter which is correctly placed 
+    def eval(self, guess):
+        for i in range(self.length):
+            if guess[i] == self.word[i]: # 2 for a letter which is correctly placed 
+                self.score[i] = 2
+            elif guess[i] in self.word:  # 1 for a letter which appears in a different position
+                self.score[i] = 1
+            else:                        # 0 for a letter which does not appear
+                self.score[i] = 0
         if self.score == [2, 2, 2, 2, 2]:
             self.isSolved = True
         return
 
     def printscore(self):
+        # TODO: look up those damn ANSI format incantations 
         # print an all-caps letter in gray for a score of 0
         # print an all-caps letter in yellow for a score of 1
         # print an all-caps letter in green for a score of 2
         return
 
+def sanitize(self, string):
+    newstring = string.strip(" ").lower()
+    return newstring
 
 def main():
-    w = Wordle(5)
+    length = 5
+    w = Wordle(length)
     w.fromFile('wordlist.txt')
+    guess = ""
+    tries = 0
     while not w.isSolved:
-        guess = input()
-        w.eval(guess)
-        w.printscore()
+        guess = input().sanitize()
+        # TODO: check length *after* sanitizing 
+        if guess in w.wordbank:
+            w.eval(guess)
+            w.printscore()
+            tries += 1
+        elif len(guess) != length:
+            print("No Guess (wrong length)")
+        else:
+            print("No Guess (not in our word bank)")
     return
 
 
