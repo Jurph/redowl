01d101d4fbd4c831595c1ee14d4494063d7ab850
Jurph <j.r.parsons@gmail.com>
2022-01-31 21:13:52 -0500

    Combine eval() and printscore() for similarity

--- redowl.py
+++ redowl.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+import random
+import datetime
 
 class Wordle(object):
-    def __init__(self, length=5):
+    def __init__(self, length=5, seed = 420):
         self.length = length
+        self.seed = seed
         self.score = [0, 0, 0, 0, 0]
         self.isSolved = False
         self.wordbank = []
@@ -13,25 +16,31 @@ class Wordle(object):
         # TODO: "filename" probably needs to be a Path or Pathlike object for robustness 
         with open(filename,'r') as fh:
             self.wordbank = fh.readlines()
+
+        random.seed(self.seed)
+        self.word = random.choice(self.wordbank)
         return
 
     def eval(self, guess):
+        scorestring = ""
+        yellow = '\x1b[1;33;40m'
+        green = '\x1b[1;32;40m'
+        dim = '\x1b[1;30;40m'
+        end = '\x1b[0m'
         for i in range(self.length):
             if guess[i] == self.word[i]: # 2 for a letter which is correctly placed 
                 self.score[i] = 2
+                scorestring += green + guess[i].upper() + end
             elif guess[i] in self.word:  # 1 for a letter which appears in a different position
                 self.score[i] = 1
+                scorestring += yellow + guess[i].upper() + end
             else:                        # 0 for a letter which does not appear
                 self.score[i] = 0
+                scorestring += dim + guess[i].upper() + end
+        print(scorestring)
         if self.score == [2, 2, 2, 2, 2]:
             self.isSolved = True
-        return
 
-    def printscore(self):
-        # TODO: look up those damn ANSI format incantations 
-        # print an all-caps letter in gray for a score of 0
-        # print an all-caps letter in yellow for a score of 1
-        # print an all-caps letter in green for a score of 2
         return
 
 def sanitize(self, string):
@@ -39,17 +48,25 @@ def sanitize(self, string):
     return newstring
 
 def main():
+    # TODO: accept command line args for initialization
+    # -d    : debug, prints answer up front
+    # -f    : fast, seeds random with (day,hour,minute,second) precision 
+
+    # Initialize the game 
+    now = datetime.datetime.now()
+    seed = now.day
     length = 5
-    w = Wordle(length)
+    w = Wordle(length, seed)
     w.fromFile('wordlist.txt')
     guess = ""
     tries = 0
+
+    # Play the game 
     while not w.isSolved:
         guess = input().sanitize()
         # TODO: check length *after* sanitizing 
         if guess in w.wordbank:
             w.eval(guess)
-            w.printscore()
             tries += 1
         elif len(guess) != length:
             print("No Guess (wrong length)")
